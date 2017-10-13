24e6c8329c2d44d70f9c023f3d160b162e7b3af2
Asetr <asetre.paul@gmail.com>
2017-10-12 21:05:29 -0700

    board initialize

--- public/board.js
+++ public/board.js
@@ -1,16 +1,26 @@
 //The chess board
-export default Class Board {
-    //Initialize the default board
-    init() {
-        createArrays()
-    }()
+export default class Board {
+    //methods: convertBounds, convertBoard, isOffBoard
+    //Initialize methods: createArrays, findOutOfBounds
 
+    //Initialize the default board
+    constructor() {
+        //values considered out of bounds
+        this.outOfBounds = []
+        //The actual playing board conatining tiles and pieces
+        this.board = []
+        //The board containing the boundaries
+        this.boundsBoard = []
+
+
+        //Start of methods
+        this.createArrays()
+        this.findOutOfBounds()
+    }
     //Create 2 arrays to represent the board
     //2 arrays length of 64, 1 array length of 120
     //Array 1 index based ex: 0..63, Array 2 index based [0..120]
     createArrays() {
-        //The actual playing board conatining pieces
-        this.board = []
         //push 64 empty the tiles into the board
         for(let i = 0; i < 8; i++) {
             for(let j = 0; j < 8; j++) {
@@ -21,10 +31,67 @@ export default Class Board {
                 this.board.push(tile)
             }
         }
-        //The board containing the boundaries
-        this.boundsBoard = []
         for(let i = 0; i < 120; i++) {
             this.boundsBoard.push(i)
         }
+    }
+
+    findOutOfBounds() {
+        this.boundsBoard.forEach(index => {
+            //Top
+            if(index <= 19) this.outOfBounds.push(index)
+            //left side
+            else if(index % 10 === 0) this.outOfBounds.push(index)
+            //right side
+            else if(index % 10 === 9) this.outOfBounds.push(index)
+            //bottom
+            else if(index > 100) this.outOfBounds.push(index)
+        })
+    }
+
+    //converts 64 index based array to 120 index based array
+    convertBounds(rankFile) {
+        //rankFile is a string ex: '0,6' => board[6]
+        //convert rankFile to an array of inegers ex: '1,8' => [1,8]
+        let parsed = rankFile.split(',').map(num => parseInt(num))
+        let rank = parsed[0]
+        let file = parsed[1]
+        let boundsIndex = (21 + file) + (rank * 10)
+        //return the index of 120 array
+        return boundsIndex
+    }
+
+    //converts index from 120 array to 64 array
+    convertBoard(index){
+        //index === boundsBoard[index]
+        let val
+        //convert depending on index inside boundsBoard
+        if(index<31) {
+            val = index-21
+        }else if(index<41) {
+            val = index-21-2
+        }else if(index<51) {
+            val = index-21-4
+        }else if(index<61) {
+            val = index-21-6
+        }else if(index<71) {
+            val = index-21-8
+        }else if(index<81) {
+            val = index-21-10
+        }else if(index<91) {
+            val = index-21-12
+        }else if(index<101) {
+            val  = index-21-14
+        }
+        //return the board index if not out of bounds
+        if(!this.isOffBoard(index)) return val
+        //return null if out of bounds
+        else return null
+    }
+
+    //returns boolean if index is considered offboard
+    isOffBoard(index) {
+        //index === boundsBoard[index]
+        return this.outOfBounds.indexOf(index) !== -1 ? true : false
     }
 }
--- test/chess-engine-tests.js
+++ test/chess-engine-tests.js
@@ -5,9 +5,34 @@ const assert = chai.assert
 import Board from '../public/board.js'
 
 describe('Board functionality', function() {
+    var testBoard
+    var board
+    var boundsBoard
+
+    beforeEach(function() {
+        testBoard = new Board
+        board = testBoard.board
+        boundsBoard = testBoard.boundsBoard
+    })
+
     it('should Initialize an empty board', function() {
-        let board = new Board
-        console.log(board)
+        assert(board.length === 64, 'Board array should have 64 tiles')
+        assert(boundsBoard.length === 120, 'boundsBoard should have 120 tiles')
+        assert(board[0].rankFile === '0,0' && board[0].piece === null, 'tile should have correct keys')
+        assert(board[63].rankFile === '7,7' && board[63].piece === null, 'tile should have correct keys')
+    })
+
+    it('should convert to bounds', function() {
+        assert(testBoard.convertBounds('0,0') === 21, 'convert rankFile to bounds board')
+        assert(testBoard.convertBounds('1,3') === 34, 'convert rankFile to bounds board')
+        assert(testBoard.convertBounds('2,0') === 41, 'convert rankFile to bounds board')
+    })
+
+    it('should convert to board', function() {
+        assert(testBoard.convertBoard(21) === 0, 'boundsBoard[21] === board[0]')
+        assert(testBoard.convertBoard(33) === 10, 'boundsBoard[33] === board[10]')
+        assert(testBoard.convertBoard(40) === null, 'boundsBoard[40] === null')
+        assert(testBoard.convertBoard(49) === null, 'boundsBoard[49] === null')
     })
 })
 
