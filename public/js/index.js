f7d377b80a96580a975d3f7479ad5757251f5b43
Asetr <asetre.paul@gmail.com>
2017-10-16 07:11:56 -0700

    react setup

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+document.addEventListener('DOMContentLoaded', () => {
+//ReactDOM.render(/* render stuff */, document.getElementById('app'))
+    }
+);
