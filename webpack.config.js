f645880424cb73f0b40dd1b5f9a7dcb696ac842c
Asetr <asetre.paul@gmail.com>
2017-10-25 02:20:36 -0700

    h

--- webpack.config.js
+++ webpack.config.js
@@ -39,13 +39,7 @@ var config = {
         new ExtractTextPlugin({
             filename: 'main.css'
         })
-    ],
-    resolveLoader: {
-        modules: [
-            'node_modules',
-            path.join(__dirname, '../node_modules'),
-        ],
-    }
+    ]
 }
 
 module.exports = config
